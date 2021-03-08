799b4bd58737c5841138ff018f188cf7618f09a4
parkjh6567 <parkjh9984@naver.com>
2021-03-08 14:20:39 +0900

    sample chat
    chatting sample code example

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+
+import Image from './Image';
+import Input from './Input';
+import Button from './Button';
+// import Spinner from './Spinner';
+
+// export { Image, Input, Button, Spinner };
+export { Image, Input, Button };
